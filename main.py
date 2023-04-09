a9ecd22f0e7170a4d0a73ab8b1b914d19334cb6a
mohit <mohitmw22@gmail.com>
2023-04-09 15:16:57 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+st.title("The Best Company")
+
+content="""
+This code is supposed to show how good the company is. The kind of people that work here, the kind of culture that can be experienced here and the kind of product that is developed here show exactly why this is the best company."""
+st.write(content)
