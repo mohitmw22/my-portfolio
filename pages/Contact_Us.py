3d280808b953b02de1dcd000d41dd55235e935c7
mohit <mohitmw22@gmail.com>
2023-04-11 22:20:15 -0400

    storing password securely

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from send_email import send_email
+import pandas
+
+df = pandas.read_csv("topics.csv")
 
 st.set_page_config(layout="wide")
 
@@ -7,11 +10,14 @@ st.title("Contact Us")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address:")
+    sel_topic = st.selectbox("What topic do you want to discuss?",
+                             df["topic"])
     raw_message = st.text_area("Your message:")
     message = f"""\
 Subject: New email from {user_email}
 
 From: {user_email}
+Topic: {sel_topic}
 {raw_message}
 """
     button = st.form_submit_button()
--- send_email.py
+++ send_email.py
@@ -1,4 +1,5 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
@@ -6,7 +7,7 @@ def send_email(message):
     port = 465
 
     username = "mmw.creator@gmail.com"
-    password = "faxdnlpyraebmbcz"
+    password = os.getenv("PASSWORD")
 
     receiver = "mmw.creator@gmail.com"
     context = ssl.create_default_context()
